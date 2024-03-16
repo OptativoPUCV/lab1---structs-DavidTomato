dfe1c8a23f51299a9865805ecc33094cf495255e
Your Name <you@example.com>
2024-03-16 22:39:54 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -71,7 +71,6 @@ arreglos en un tercer arreglo también ordenado de menor a mayor.
 */
 void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
                        int result[]) {
-  // = (int *)malloc((size1 + size2) * sizeof(int));
   int x = 0, y = 0, z = 0;
   while (x < size1 && y < size2){
     if (arr1[x] <= arr2[y]){
@@ -102,7 +101,12 @@ Descripción: Escribe una función que tome un arreglo y su tamaño,
 y luego devuelva 1 si el arreglo está ordenado en orden ascendente,
   0 si no está ordenado, y -1 si está ordenado en orden descendente.
 */
-int checkSorted(int arr[], int size) { return -2; }
+int checkSorted(int arr[], int size) {
+  int oa = 1, od = 1;
+  for (int i = 0; i < size - 1; i++)
+  
+  
+  return -2; }
 
 /*
 Ejercicio 6: Información de una Biblioteca
